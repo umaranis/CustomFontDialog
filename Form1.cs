2d3cdd2c31b5fffc4a38caa6d896c4cafbdbcdf1
Syed Umar Anis <umarsua@hotmail.com>
2013-10-12 23:09:33 +0400

    Initial

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomFontDialog
+{
+    public partial class Form1 : Form
+    {
+        FontDialog f;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            f = new FontDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            f.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            f.AddFontToRecentList(FontFamily.GenericSansSerif);
+            f.AddFontToRecentList(FontFamily.GenericSerif);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            f.Font = this.Font;
+        }
+    }
+}
